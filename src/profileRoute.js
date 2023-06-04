ae02bcd96a1c107d09c3557e4c0d45b8a3ae49f8
danurrizad <danurrizad242@gmail.com>
2023-06-05 00:29:26 +0700

    fix: import helpers uploadimage for updateProfile

--- src/profileRoute.js
+++ src/profileRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
 import * as controller from "./profileController.js";
 import * as auth from "./middlewares/auth.js";
-import {uploadimage} from "./helpers/uploadimage.js"
+import {upload as uploadimage} from "./helpers/uploadimage.js"
 
 const router = express.Router();
 
 router.patch(
   "/update",
-  uploadimage.upload.single('foto'),
+  uploadimage.single('foto'),
   auth.newAuthenticator(),
   auth.newRoleAuthorizer("STUDENT"),
   controller.updateProfile
