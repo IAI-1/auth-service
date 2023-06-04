75852dab77593bf36d0cbec4a8c296ed8905f07c
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-06-04 11:57:35 +0700

    [modify] user model schema

--- src/userModel.js
+++ src/userModel.js
@@ -1,30 +1,42 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            required: true,
+            enum: ["STUDENT", "ADMIN"],
+            default: "STUDENT",
+        },
+        nama: {
+            type: String,
+            required: false,
+        },
+        niu: {
+            type: String,
+            required: false,
+        },
+        prodi: {
+            type: String,
+            required: false,
+        },
+        foto: {
+            type: String,
+            required: false,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    role: {
-      type: String,
-      required: true,
-      enum: ['STUDENT', 'ADMIN'],
-      default: 'STUDENT',
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
